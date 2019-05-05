fb4235dfb42e598d764ba6415b619e367bd6bd64
Austin T <austin.taranto@gmail.com>
2019-05-05 16:13:29 -0400

    rules for guessing

---
+++ Archive/logic.js
@@ -0,0 +1,72 @@
+var letters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+        // array of words
+        var gameWords: ["snake", "liquid", "greyfox", "bigboss", "ocelot"];
+
+            randomWord(["snake", "liquid", "greyfox", "bigboss", "ocelot"]);
+
+                //picking random word from array
+                var word = gameWords[Math.floor(Math.random() * gameWords.length)];
+
+                var getBlanks = [];
+                for (var i = 0; i < word.length; i++){
+                    getBlanks[i] = "_";
+                }
+
+                var fillBlanks = word.length;
+
+                while (fillBlanks > 0) {
+                    alert getBlanks.join(" ");
+                }
+
+               var guess = prompt("Break The Codec.");
+               if (guess === null){
+                   break;
+               } else if (guess.length !== 1) {
+                   alert("Enter Letter.")
+               } else {
+                   for (var g = 0; g < word.length; g++){
+                       if (word[g] === guess) {
+                           getBlanks[g] = guess; fillBlanks;
+                       }
+                   }
+               }
+                
+
+        setupRound
+        word
+        guessesLeft
+        wrongGuesses
+        puzzleState
+
+
+
+    isCorrectGuess('snake', 's');
+    if (true){
+        return correct;
+    else (false)
+        return incorrect;
+
+    console.log(isCorrectGuess(s));
+
+    //returns ["_", "_", "_", "_", "_"]
+    getBlanks("snake");
+
+    function getBlanks(_){
+        if(letters(_)) == 's'{
+            return true;
+        }
+        if (letters(_)) == "a"{
+            return false;
+        }
+    }
+    //returns ["_", "n", "_", "_", "_"]
+    fillBlanks("snake", ["_", "_", "_", "_", "_", "n"];
+
+
+
+    randomWord function
+    isCorrectGuess function
+    getBlanks function
+    fillBlanks function
+    gameWords variable
