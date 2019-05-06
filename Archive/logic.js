d62a6710c3db6e1e1b9c50340eb293835997ee83
Austin T <austin.taranto@gmail.com>
2019-05-05 20:29:21 -0400

    a save thus far

--- Archive/logic.js
+++ Archive/logic.js
@@ -18,33 +18,17 @@ var letters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "
                     var guess = prompt("Break The Codec.");
                         if (guess === null){
                         break;
-                            } else if (guess.length !== 1) {
-                                alert("Enter Letter.")
-                                    } else {
-                                        for (var g = 0; g < word.length; g++){
-                                            if (word[g] === guess) {
-                                                getBlanks[g] = guess; fillBlanks;
+                        } else if (guess.length !== 1) {
+                        alert("Enter Letter.")
+                        } else {
+                        for (var g = 0; g < word.length; g++){
+                        if (word[g] === guess) {
+                        getBlanks[g] = guess; fillBlanks;
                        }
                    }
                }
 
             }
-
-             // create alphabet list
-            var buttons = function () {
-            myButtons = document.getElementById('buttons');
-            letters = document.createElement('ul');
-
-            for (var i = 0; i < alphabet.length; i++) {
-            letters.id = 'alphabet';
-            list = document.createElement('li');
-            list.id = 'letter';
-            list.innerHTML = alphabet[i];
-            check();
-            myButtons.appendChild(letters);
-            letters.appendChild(list);
-            }
-        }
 
                alert(getBlanks.join(" "));
                alert("Mission Accomplished " + word);
