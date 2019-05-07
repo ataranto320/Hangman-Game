6e069d38863d09aa3111034daf2614b8a7f79c24
Austin T <austin.taranto@gmail.com>
2019-05-07 11:52:43 -0400

    saved last nights work

--- Archive/logic.js
+++ Archive/logic.js
@@ -1,161 +1,169 @@
-var letters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// var letters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
         // array of words
-        var gameWords: ["snake", "liquid", "greyfox", "bigboss", "ocelot"];
 
-        var word;
-        var guess;
-        var guesses = [ ];
-        var lives;
-        var counter;
-        var space;
+        var gameWords = ['snake', 'liquid', 'greyfox', 'bigboss', 'ocelot']
 
-        //elements
-        var guessesLeft = document.getElementById("guessesLeft");
-        var randomWord = document.getElementById("randomWord");
-        var getBlanks = document.getElementById("getBlanks");
-        var fillBlanks = document.getElementById("fillBlanks");
-
-        //try to create letter list
-        var buttons = function() {
-            myButtons = document.getElementById("buttons");
-            letters = document.getElementById("ul");
+        function randomWord(words){
+            var randomWord = gameWords[Math.floor(Math.random() * gameWords.length)];
         }
 
-            for (var i = 0; i < letters.length; i++) {
-                letters.id = "letters";
-                list = document.createElement("li");
-                list.id = "letter";
-                list.innerHTML = letters[i];
-                check();
-                myButtons.appendChild(letters);
-                letters.appendChild(list);
-            }
+    //     var word;
+    //     var guess;
+    //     var guesses = [ ];
+    //     var lives;
+    //     var counter;
+    //     var space;
+
+    //     //elements
+    //     var guessesLeft = document.getElementById("guessesLeft");
+    //     var randomWord = document.getElementById("randomWord");
+    //     var getBlanks = document.getElementById("getBlanks");
+    //     var fillBlanks = document.getElementById("fillBlanks");
+
+    //     //try to create letter list
+    //     var buttons = function() {
+    //         myButtons = document.getElementById("buttons");
+    //         letters = document.getElementById("ul");
+    //     }
+
+    //         for (var i = 0; i < letters.length; i++) {
+    //             letters.id = "letters";
+    //             list = document.createElement("li");
+    //             list.id = "letter";
+    //             list.innerHTML = letters[i];
+    //             check();
+    //             myButtons.appendChild(letters);
+    //             letters.appendChild(list);
+    //         }
+
+    //         //guesses left
+            
 
 
-                //picking random word from array
-                var randomWord = gameWords[Math.floor(Math.random() * gameWords.length)];
+    //             //picking random word from array
+                
 
-                var getBlanks = [];
-                for (var i = 0; i < word.length; i++){
-                    getBlanks[i] = "_";
-                }
+    //             var getBlanks = [];
+    //             for (var i = 0; i < word.length; i++){
+    //                 getBlanks[i] = "_";
+    //             }
 
-                var fillBlanks = word.length;
+    //             var fillBlanks = word.length;
 
-                while (fillBlanks > 0) {
+    //             while (fillBlanks > 0) {
 
-                    var guess = prompt("Break The Codec.");
-                        if (guess === null){
-                        break;
-                        } else if (guess.length !== 1) {
-                        alert("Enter Letter.")
-                        } else {
-                        for (var g = 0; g < word.length; g++){
-                        if (word[g] === guess) {
-                        getBlanks[g] = guess; fillBlanks;
-                       }
-                   }
-               }
+    //                 var guess = prompt("Break The Codec.");
+    //                     if (guess === null){
+    //                     break;
+    //                     } else if (guess.length !== 1) {
+    //                     alert("Enter Letter.")
+    //                     } else {
+    //                     for (var g = 0; g < word.length; g++){
+    //                     if (word[g] === guess) {
+    //                     getBlanks[g] = guess; fillBlanks;
+    //                    }
+    //                }
+    //            }
 
-            }
+    //         }
 
-               alert(getBlanks.join(" "));
-               alert("Mission Accomplished " + word);
+    //            alert(getBlanks.join(" "));
+    //            alert("Mission Accomplished " + word);
 
-               var gameWords = function (){
+    //            var gameWords = function (){
                    
-               };
+    //            };
 
-               function setupRound(word){
-               var word = pickWord();
-               var getBlanks = setupGetBlanks(word);
-               var fillBlanks = word.length;
-               var pickWord = function() {
+    //            function setupRound(word){
+    //            var word = pickWord();
+    //            var getBlanks = setupGetBlanks(word);
+    //            var fillBlanks = word.length;
+    //            var pickWord = function() {
 
-               };
+    //            };
 
-               function updateRound(word){
+    //            function updateRound(word){
 
-               }
+    //            }
 
-               setupGetBlanks = function (word) {
-                document.getElementById(getBlanks);
-               };
+    //            setupGetBlanks = function (word) {
+    //             document.getElementById(getBlanks);
+    //            };
 
-               showPlayerProgress = function (getBlanks){
-                document.getElementById(fillBlanks);
-               };
+    //            showPlayerProgress = function (getBlanks){
+    //             document.getElementById(fillBlanks);
+    //            };
 
-               updateRound = function (guess, word, getBlanks) {
-                document.getElementById()
-               };
-
-
-
-               setupRound
-               word
-               guessesLeft
-               wrongGuesses
-               puzzleState
-
-               var hasWon = ["Mission Accomplished"];
-               var hasLost = ["Game Over"];
-               isEndOfRound
-               setupGame
-               startNewRound
-               myGame
+    //            updateRound = function (guess, word, getBlanks) {
+    //             document.getElementById()
+    //            };
 
 
-               var audioElement = document.createElement("audio");
-               audioElement.setAttribute(src="https://www.youtube.com/watch?v=5105DeorjrY");
+
+    //            setupRound
+    //            word
+    //            guessesLeft
+    //            wrongGuesses
+    //            puzzleState
+
+    //            var hasWon = ["Mission Accomplished"];
+    //            var hasLost = ["Game Over"];
+    //            isEndOfRound
+    //            setupGame
+    //            startNewRound
+    //            myGame
+
+
+    //            var audioElement = document.createElement("audio");
+    //            audioElement.setAttribute(src="https://www.youtube.com/watch?v=5105DeorjrY");
                 
 
         
 
 
 
-    isCorrectGuess('snake', 's');
-    if (true){
-        return fillBlanks;
-    else (false)
-        return getBlanks;
+    // isCorrectGuess('snake', 's');
+    // if (true){
+    //     return fillBlanks;
+    // else (false)
+    //     return getBlanks;
 
-    isCorrectGuess('snake', 'n');
-    if (true){
-        return fillBlanks;
-    else (false)
-        return getBlanks;
+    // isCorrectGuess('snake', 'n');
+    // if (true){
+    //     return fillBlanks;
+    // else (false)
+    //     return getBlanks;
 
-    isCorrectGuess('snake', 'a');
-    if (true){
-        return fillBlanks;
-    else (false)
-        return getBlanks;
+    // isCorrectGuess('snake', 'a');
+    // if (true){
+    //     return fillBlanks;
+    // else (false)
+    //     return getBlanks;
 
-    isCorrectGuess('snake', 'k');
-    if (true){
-        return fillBlanks;
-    else (false)
-        return getBlanks;
+    // isCorrectGuess('snake', 'k');
+    // if (true){
+    //     return fillBlanks;
+    // else (false)
+    //     return getBlanks;
 
-    isCorrectGuess('snake', 'e');
-    if (true){
-        return fillBlanks;
-    else (false)
-        return getBlanks;
-
-
+    // isCorrectGuess('snake', 'e');
+    // if (true){
+    //     return fillBlanks;
+    // else (false)
+    //     return getBlanks;
 
 
 
-    function getBlanks(_){
-        if(letters(_)) == 's'{
-            return true;
-        }
-        if (letters(_)) == "a"{
-            return false;
-        }
+
+
+    // function getBlanks(_){
+    //     if(letters(_)) == 's'{
+    //         return true;
+    //     }
+    //     if (letters(_)) == "a"{
+    //         return false;
+    //     }
 
 
 
